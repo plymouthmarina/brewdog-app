07f403a260fad62d1219685d66cd2d0fc689ff88
plymouthmarina <marina.trethewey@gmail.com>
2023-03-04 23:17:52 +0000

    Added Beer interface and fetching data from punkapi.

--- pages/discover.tsx
+++ pages/discover.tsx
@@ -1,7 +1,24 @@
-export default function Discover() {
+import { NextPage } from "next";
+import { GetStaticProps } from "next";
+import { Beer } from "@/types/Beer";
+
+const Discover: NextPage<{ beers: Beer[] }> = ({ beers }) => {
     return (
         <>
             <h1>Discover</h1>
         </>
-    )
-}
+    );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const response = await fetch("https://api.punkapi.com/v2/beers");
+    const results: Beer[] = await response.json();
+
+    return {
+        props: {
+            beers: results,
+        },
+    };
+};
+
+export default Discover;
