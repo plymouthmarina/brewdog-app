879d67d4523fe2d1ec5f116b7360a01eb536f9ac
plymouthmarina <marina.trethewey@gmail.com>
2023-03-05 11:38:35 +0000

    Added sorting functionality by ABV

--- components/GalleryItem.tsx
+++ components/GalleryItem.tsx
@@ -7,7 +7,7 @@ type GalleryItemProps = {
 };
 
 const GalleryItem = ({ beer }: GalleryItemProps) => {
-    const { image_url, name, tagline } = beer;
+    const { image_url, name, tagline, abv } = beer;
 
     return (
         <div className="flex flex-col border-2 border-tertiary-light">
@@ -23,6 +23,10 @@ const GalleryItem = ({ beer }: GalleryItemProps) => {
             </div>
             <div className="flex-1 bg-tertiary-light p-4 text-base text-tertiary-dark lg:text-lg">
                 <h2 className="font-bold">{name}</h2>
+                <p>
+                    <span className="font-semibold text-secondary">ABV: </span>
+                    {abv}
+                </p>
                 <p>{tagline}</p>
             </div>
         </div>
